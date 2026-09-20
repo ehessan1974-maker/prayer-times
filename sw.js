681bce51c047788d0ec85b316b7304f4dec489c5
Z User <z@container>
2026-09-20 08:05:18 +0000

    v20.4: إصلاح جلب content.json + إعادة المحاولة التلقائية
    الإصلاحات:
    1. fetchContentJSONViaXHR: إضافة إعادة محاولة تلقائية (3 مرات)
    2. _nativeContentCallback: إعادة المحاولة عبر XHR عند الفشل
    3. تقليل مهلة XHR من 15 إلى 10 ثوانٍ
    4. رفع الإصدار: 20.4, versionCode 23, SW_CACHE v22

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    على NativeBridge.getDeviceId بدلاً من هذا).
    ======================================================================== */
 
-var SW_CACHE = "prayer-times-v21";
+var SW_CACHE = "prayer-times-v22";
 var APP_SHELL = [
   "./",
   "./prayer-times.html",
