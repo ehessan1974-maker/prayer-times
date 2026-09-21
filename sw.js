d6c8553d4c03772de71fca49d95f398cf808a40c
Z User <z@container>
2026-09-21 22:39:29 +0000

    v22.2: إصلاح زر التحديث + رابط البرنامج في content.json

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    على NativeBridge.getDeviceId بدلاً من هذا).
    ======================================================================== */
 
-var SW_CACHE = "prayer-times-v30";
+var SW_CACHE = "prayer-times-v31";
 var APP_SHELL = [
   "./",
   "./prayer-times.html",
