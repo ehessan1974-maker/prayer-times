e035a13ed78bc30f5caa9977f6d60b4f3d86369b
Z User <z@container>
2026-09-22 17:46:54 +0000

    v24.0: ألوان مغايرة للأزرار + تأثيرات تحويم وضغط
    1. الزر النشط (المضغوط): ذهبي #d4af37 + نص أسود + حدود داكنة
    2. الأزرار غير النشطة: أبيض/أخضر داكن حسب الوضع
    3. تأثير التحويم (hover): opacity 0.75 + تكبير 1.05
    4. تأثير الضغط (active): opacity 0.6 + تصغير 0.95
    5. انتقال سلس (transition) على كل الأزرار
    
    رفع الإصدار: 24.0, versionCode 45, SW_CACHE v45

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    على NativeBridge.getDeviceId بدلاً من هذا).
    ======================================================================== */
 
-var SW_CACHE = "prayer-times-v44";
+var SW_CACHE = "prayer-times-v45";
 var APP_SHELL = [
   "./",
   "./prayer-times.html",
