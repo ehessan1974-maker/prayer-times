fba5b4bdfe490ca26ce5fec4327f73d426c34e3f
Z User <z@container>
2026-09-22 00:15:46 +0000

    v22.6: إعادة توليد الأيقونة + إضافة roundIcon
    1. إعادة توليد كل أيقونات الجامع بكل الأحجام
    2. إضافة android:roundIcon للمستخدمين الذين يستخدمون أيقونات دائرية
    3. رفع versionCode إلى 35 لإجبار أندرويد على تحديث الأيقونة
    4. تحديث AndroidManifest.xml في android-studio و android-app

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    على NativeBridge.getDeviceId بدلاً من هذا).
    ======================================================================== */
 
-var SW_CACHE = "prayer-times-v34";
+var SW_CACHE = "prayer-times-v35";
 var APP_SHELL = [
   "./",
   "./prayer-times.html",
